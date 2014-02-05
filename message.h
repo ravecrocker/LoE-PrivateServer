8930e1dce4d6f9ec0955150ffd1d89f83b9fa8de
mlkj <barrdetwix@gmail.com>
2014-02-05 23:07:52 +0100

    Fix setStat setMaxStat to work on others.
    Also remove the worn items by default.

--- message.h
+++ message.h
@@ -84,8 +84,10 @@ void sendNetviewInstantiate(Player* src, Player* dst);
 void sendNetviewRemove(Player* player, quint16 netviewId);
 void sendSetStatRPC(Player* player, quint8 statId, float value);
 void sendSetMaxStatRPC(Player* player, quint8 statId, float value);
-void sendWornRPC(Player* player, QList<WearableItem>& worn);
-void sendWornRPC(Player* wearing, Player* dest, QList<WearableItem>& worn);
+void sendSetStatRPC(Player* affected, Player* dest, quint8 statId, float value); // Set stat of affected on client dest
+void sendSetMaxStatRPC(Player* affected, Player* dest, quint8 statId, float value); // Set stat of affected on client dest
+void sendWornRPC(Player* player, QList<WearableItem>& worn); // Send items worn by player to himself
+void sendWornRPC(Player* wearing, Player* dest, QList<WearableItem>& worn); // Send items worn by wearing to dest
 void sendInventoryRPC(Player* player, QList<InventoryItem>& inv, QList<WearableItem>& worn, quint32 nBits);
 void sendSkillsRPC(Player* player, QList<QPair<quint32, quint32> >& skills);
 void sendLoadSceneRPC(Player* player, QString sceneName);
