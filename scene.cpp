9f5c7202757702725554e687306046f497d69f68
theCoolkal <cp19cb97@hotmail.com>
2014-03-31 22:59:19 +0100

    Added more commands

--- scene.cpp
+++ scene.cpp
@@ -17,7 +17,7 @@ Scene::Scene(QString sceneName)
 Scene* findScene(QString sceneName)
 {
     for (int i=0; i<win.scenes.size(); i++)
-        if (win.scenes[i].name == sceneName)
+        if (win.scenes[i].name.toLower() == sceneName.toLower())
             return &win.scenes[i];
 
     return new Scene("");
@@ -27,7 +27,7 @@ Vortex findVortex(QString sceneName, quint8 id)
 {
     Scene scene(sceneName);
     for (int i=0; i<win.scenes.size(); i++)
-        if (win.scenes[i].name == sceneName)
+        if (win.scenes[i].name .toLower()== sceneName.toLower())
             scene = win.scenes[i];
 
     for (int i=0; i<scene.vortexes.size(); i++)
