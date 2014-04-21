3494df6ed0a889b6a4c208ac03739e6ab146504a
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-04-22 00:05:16 +0200

    Nerf some mobs a little

--- src/mobStats.cpp
+++ src/mobStats.cpp
@@ -2,4 +2,4 @@
 
 unsigned defaultMaxHealth[] = {75, 30, 100, 100, 75, 100, 100, 100};
 
-float defaultDefense[] = {1, 1, 1.5, 10, 1, 1.25, 1, 1.25};
+float defaultDefense[] = {1, 1, 1.5, 10, 0.75, 1, 0.85, 1};
