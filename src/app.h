a47592970cc6037f959287b3e46dc872954bb8dd
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-08-12 14:29:26 +0200

    Bump version to 0.6

--- src/app.h
+++ src/app.h
@@ -29,7 +29,7 @@
 #include "CoreFoundation/CoreFoundation.h"
 #endif
 
-#define VERSIONSTRING "v0.5.3-beta1"
+#define VERSIONSTRING "v0.6"
 
 #define GAMEDATAPATH "data/data/"
 #define PLAYERSPATH "data/players/"
